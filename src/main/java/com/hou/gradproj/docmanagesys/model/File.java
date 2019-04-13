5a3bb452ca0f3cb40387a4e5cf57972571af349c
houlx <houlxssdut@gmail.com>
2019-04-14 00:42:56 +0800

    show file list, upload, delete & download api

--- src/main/java/com/hou/gradproj/docmanagesys/model/File.java
+++ src/main/java/com/hou/gradproj/docmanagesys/model/File.java
@@ -1,20 +1,18 @@
 package com.hou.gradproj.docmanagesys.model;
 
 import com.hou.gradproj.docmanagesys.model.audit.UserDateAudit;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.math.BigInteger;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "files")
+@Data
 @NoArgsConstructor
-@Getter
-@Setter
 public class File extends UserDateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,24 +22,20 @@ public class File extends UserDateAudit {
     @Size(max = 140)
     private String name;
 
-    @NotBlank
+//    @NotBlank
     private BigInteger size;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "file_type_id", nullable = false)
+    @JoinColumn(name = "type_id", nullable = false)
     private FileType type;
 
     @NotBlank
     private String path;
 
-    @NotBlank
-    private Boolean deleted;
-
-    public File(@NotBlank @Size(max = 140) String name, @NotBlank BigInteger size, FileType type, @NotBlank String path, @NotBlank Boolean deleted) {
+    public File(@NotBlank @Size(max = 140) String name, @NotBlank BigInteger size, FileType type, @NotBlank String path) {
         this.name = name;
         this.size = size;
         this.type = type;
         this.path = path;
-        this.deleted = deleted;
     }
 }
